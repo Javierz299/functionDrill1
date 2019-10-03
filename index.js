b659419643bc809ae4ddc77e4d1e9a85b1f1094b
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-10-03 16:01:40 -0400

    add new error and try/catch

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function yearOfBirth(age){
-  
+  if(age < 0){
+    throw new Error('Age can not be negative');
+  }
+
   return 2019 - age;
 }
 
@@ -10,5 +13,11 @@ function createGreeting(name,age){
   return `Hi, my ${name} is Chris and I'm ${age} years old`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+
+
+try {
+  const greeting1 = createGreeting('Chris',29);
+  console.log(greeting1);
+} catch(e) {
+  console.error(e.message);
+}
