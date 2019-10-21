448d9e2881655d3eafa861b016f2e0e24663c227
Javierz299 <47990570+Javierz299@users.noreply.github.com>
2019-10-21 15:58:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,34 @@ try {
 } catch(e) {
   console.error(e.message);
 }
+////////////////////bottom half complete/////////////////////////////
+'use strict';
+
+function getYearOfBirth(age) {
+  if(age < 0){
+    throw new Error ('Age can not be negative');
+  }
+
+
+  return 2019 - age;
+}
+
+function createGreeting (name, age){
+  const yearOfBirth = getYearOfBirth(age);
+  if (name === undefined || age === undefined) {
+    throw new Error ('Arguments not valid');
+    // typeof age !== isNan
+  } if(typeof name !== 'string' ||  typeof age !== 'number'){
+    throw new TypeError('name must be a string and age must be a number');
+  }
+  return `Hi, my name is ${name} and I'm  ${age} years old. I was born in ${yearOfBirth}`;
+  
+}
+
+
+try {
+  const greeting1 = createGreeting('chris',29);
+  console.log(greeting1);
+} catch(e){
+  console.log(e.message);
+}
